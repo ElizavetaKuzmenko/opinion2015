25c0d01bcaebde5424f4fe3fe9687d5840ba44b6
sereni <sereni.nm@gmail.com>
2015-10-18 21:23:57 +0300

    add todo for direct speech

---
+++ direct_speech.py
@@ -0,0 +1,11 @@
+__author__ = 'Sereni'
+"""
+A direct speech module draft
+"""
+
+# define quote chars
+# find a huge chunk of text in quotes
+# find a noun before and after it
+# define links between nouns and speech chunks
+# decide which noun looks more like a speaker
+# extend speakers to noun phrases like in tomita
