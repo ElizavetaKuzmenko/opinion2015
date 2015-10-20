d16c50cf323f3eac52a3761150ac03d6c5dd3601
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-21 00:22:19 +0300

    transforming categorical features

--- ml.py
+++ ml.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 __author__ = 'liza'
 
-import os, re
+import os, re, sys
 from xml.etree import ElementTree as et
 from create_features import features
 from sklearn.svm import LinearSVC
+from sklearn.preprocessing import OneHotEncoder
 #from sklearn.linear_model import LogisticRegression
 #clf2 = LogisticRegression()
 #from sklearn.linear_model import SGDClassifier
@@ -33,6 +34,11 @@ OTHER = 0
 
 NEWLINE = re.compile('(\n|\r)+')
 
+# preprocessing for categorical features -- POS
+enc = OneHotEncoder()
+categorical_integers = {'NOUN': 0, 'ADJF': 1, 'ADJS': 2, 'COMP': 3, 'VERB': 4, 'INFN': 5, 'PRTF': 6, 'PRTS': 7, 'GRND': 8,
+                        'NUMR': 9, 'ADVB': 10, 'NPRO': 11, 'PRED': 12, 'PREP': 13, 'CONJ': 14, 'PRCL': 15, 'INTJ': 16, 'UNK': 17}
+
 def replace_newlines(string):
     """
     Merge newlines and replace with END_OF_PARAGRAPH marker
@@ -45,15 +51,23 @@ def replace_newlines(string):
 def parse_gold(TABLE):
     features_gold = []
     labels = []
+    categorical = []
     with open(TABLE) as t:
-        for line in t[1:]:
+        for line in t:
+            if '-' in line:
+                continue
             data = line.strip().split('\t')
             labels.append(data[2])
             # TODO: cut the feature with quotes to avoid over-training
-            features_gold.append(data[3:])
+            categorical_features = [categorical_integers[x] for x in data[6:13]]
+            categorical.append(categorical_features)
+            features_gold.append(data[3:6] + categorical_features + data[13:])
+    enc.fit(categorical)
+    for i in range(len(features_gold)):
+        features_gold[i] = [int(x) for x in features_gold[i][:3]] + enc.transform([features_gold[i][3:10]]) + [int(x) for x in features_gold[i][10:]]
     return features_gold, labels
 
-
+sys.stdout.write('Training classifier...')
 clf1 = LinearSVC()
 # fitting the classifier to our goldset data
 features_gold, labels = parse_gold(TABLE)
@@ -77,6 +91,9 @@ for filename in os.listdir(DIRNAME):
 
             for token in data:
                 token_features = data.split('\t')[2:]
+                categorical_features = [categorical_integers[x] for x in token_features[6:13]]
+                token_features = [int(x) for x in token_features[:3]] + enc.transform([categorical_features]) + [int(x) for x in token_features[10:]]
+
                 category = clf1.predict(token_features)
                 if category == OPEN:
                     new_file.write('<UYUYGU>' + token + ' ')
