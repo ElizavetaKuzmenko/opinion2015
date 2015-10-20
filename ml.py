b00b70f85bb147e2d77da76ad1a75c1ec5322232
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-21 01:33:20 +0300

    transforming categorical features in another way

--- ml.py
+++ ml.py
@@ -6,6 +6,7 @@ from xml.etree import ElementTree as et
 from create_features import features
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction import DictVectorizer
 #from sklearn.linear_model import LogisticRegression
 #clf2 = LogisticRegression()
 #from sklearn.linear_model import SGDClassifier
@@ -36,6 +37,7 @@ NEWLINE = re.compile('(\n|\r)+')
 
 # preprocessing for categorical features -- POS
 enc = OneHotEncoder()
+vec = DictVectorizer()
 categorical_integers = {'NOUN': 0, 'ADJF': 1, 'ADJS': 2, 'COMP': 3, 'VERB': 4, 'INFN': 5, 'PRTF': 6, 'PRTS': 7, 'GRND': 8,
                         'NUMR': 9, 'ADVB': 10, 'NPRO': 11, 'PRED': 12, 'PREP': 13, 'CONJ': 14, 'PRCL': 15, 'INTJ': 16, 'UNK': 17}
 
@@ -51,20 +53,26 @@ def replace_newlines(string):
 def parse_gold(TABLE):
     features_gold = []
     labels = []
-    categorical = []
+    #categorical = []
     with open(TABLE) as t:
         for line in t:
             if '-' in line:
                 continue
             data = line.strip().split('\t')
             labels.append(data[2])
+            feature_vec = {}
+            for i in range(len(data[3:])):
+                feature_vec[i] = data[i]
             # TODO: cut the feature with quotes to avoid over-training
-            categorical_features = [categorical_integers[x] for x in data[6:13]]
-            categorical.append(categorical_features)
-            features_gold.append(data[3:6] + categorical_features + data[13:])
-    enc.fit(categorical)
-    for i in range(len(features_gold)):
-        features_gold[i] = [int(x) for x in features_gold[i][:3]] + enc.transform([features_gold[i][3:10]]) + [int(x) for x in features_gold[i][10:]]
+            #categorical_features = [categorical_integers[x] for x in data[6:13]]
+            #categorical.append(categorical_features)
+            #features_gold.append(data[3:6] + categorical_features + data[13:])
+            features_gold.append(feature_vec)
+    #enc.fit(categorical)
+    #print(enc.transform([[13, 5, 4, 0, 0, 17, 17]]).toarray())
+    #for i in range(len(features_gold)):
+    #    features_gold[i] = [int(x) for x in features_gold[i][:3]] + enc.transform([features_gold[i][3:10]]) + [int(x) for x in features_gold[i][10:]]
+    features_gold = vec.fit_transform(features_gold).toarray()
     return features_gold, labels
 
 sys.stdout.write('Training classifier...')
@@ -91,8 +99,12 @@ for filename in os.listdir(DIRNAME):
 
             for token in data:
                 token_features = data.split('\t')[2:]
-                categorical_features = [categorical_integers[x] for x in token_features[6:13]]
-                token_features = [int(x) for x in token_features[:3]] + enc.transform([categorical_features]) + [int(x) for x in token_features[10:]]
+                token_vec = {}
+                for i in range(len(token_features)):
+                    token_vec[i] = token_features[i]
+                token_features = vec.fit_transform(token_vec).toarray()
+                #categorical_features = [categorical_integers[x] for x in token_features[6:13]]
+                #token_features = [int(x) for x in token_features[:3]] + enc.transform([categorical_features]) + [int(x) for x in token_features[10:]]
 
                 category = clf1.predict(token_features)
                 if category == OPEN:
