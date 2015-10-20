417841319c8728329a7c75687366d4479f569122
sereni <sereni.nm@gmail.com>
2015-10-21 00:56:23 +0300

    add sort of working indirect speech

--- crude_tagger.py
+++ crude_tagger.py
@@ -78,6 +78,98 @@ def categorize_direct(tokens):
     return categorized_tokens
 
 
+def find_comma(array):
+    """
+    Finds a comma in a given list of tokens. Returns index or None.
+    Used to see if we hadn't missed a comma while not looking, and suddenly deciding to look again.
+    """
+    for i in range(len(array)):
+        if array[i][0] == ',':
+            return i
+    return None
+
+def categorize_indirect(tokens):
+
+    categorized = []
+
+    mark_commas = True  # set if in search for the next phrase; false if searching for limits of current phrase
+    seen_comma = None  # None or an index of the last good comma (ask me for definition of good)
+    last_EOS = -1  # marks last seen end of sentence marker
+    in_quotes = False
+    searching_for_next_comma = False  # when found a verb and only need the next comma
+
+    for i in range(len(tokens)):
+
+        current_token = tokens[i]  # for debugging
+        categorized.append(tokens[i])  # transfer token unchanged first
+
+        if not in_quotes:
+
+            if tokens[i][0] == ',' and mark_commas:
+
+                if searching_for_next_comma:
+
+                    searching_for_next_comma = False
+                    mark_commas = False  # from here on, look for exit markers and do not remember any commas
+
+                seen_comma = i
+
+            elif tokens[i][0] == END_OF_SENTENCE:
+                last_EOS = i
+
+            elif tokens[i][1] == 1:
+                in_quotes = True  # if we got inside direct speech, don't do anything until it's over
+
+                # we've been searching for an end of speech and stumbled into direct speech
+                # means we've gone too far, and let's chop it off at the previous sentence
+                if not mark_commas:
+                    categorized[seen_comma+1] = (categorized[seen_comma+1][0], OPEN)  # start
+                    categorized[last_EOS-1] = (categorized[last_EOS-1][0], CLOSE)  # end
+                    mark_commas = True
+                    seen_comma = find_comma(tokens[last_EOS: i])
+
+                else:
+                    seen_comma = None
+
+            elif tokens[i][0] in VERBS:
+
+                current_verb = (tokens[i], i)  # for debugging
+
+                if seen_comma:
+
+                    if not mark_commas:  # found a verb when looking for speech end; chop speech off at last EOS
+                        categorized[seen_comma+1] = (categorized[seen_comma+1][0], OPEN)  # start
+                        categorized[last_EOS-1] = (categorized[last_EOS-1][0], CLOSE)  # end
+                        mark_commas = True
+                        seen_comma = find_comma(tokens[last_EOS: i])
+
+                    # an actual good match, define phrase
+                    else:
+                        # find limits and set them to start and end
+                        categorized[last_EOS+1] = (categorized[last_EOS+1][0], OPEN)  # start
+                        categorized[seen_comma-1] = (categorized[seen_comma-1][0], CLOSE)  # end
+                        seen_comma = None
+
+                else:
+                    searching_for_next_comma = True  # set a marker to let the next comma know it's the last
+
+            # exit markers
+
+            elif tokens[i][0] == END_OF_PARAGRAPH.strip() or tokens[i][0] == END_OF_FILE.strip():
+
+                if seen_comma:
+                    categorized[seen_comma+1] = (categorized[seen_comma+1][0], OPEN)
+                    categorized[i] = (categorized[i-2][0], CLOSE)  # -2 because before EOF/EOP there's normally an EOS
+                    seen_comma = None
+                    mark_commas = True
+
+        else:  # look if it's time to close the quotes and keep searching
+            if tokens[i][1] == 2:
+                in_quotes = False
+
+    return categorized
+
+
 def replace_newlines(string):
     """
     Merge newlines and replace with END_OF_PARAGRAPH marker
@@ -103,6 +195,7 @@ if __name__ == '__main__':
 
     dir_path = os.path.join(os.getcwd(), DIRNAME)
     output = open('categories.csv', 'w')
+    load_verbs()
     # iterate through data folder
     for filename in os.listdir(dir_path):
         if filename.endswith('xml'):
@@ -118,7 +211,8 @@ if __name__ == '__main__':
                 tokenized_text = tokenize(text)
 
                 # categorize_direct tokens
-                categorized = categorize_direct(tokenized_text)
+                tokens = categorize_direct(tokenized_text)
+                categorized = categorize_indirect(tokens)
 
                 # write output
                 for (token, category) in categorized:
