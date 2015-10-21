08d1ab28e3702d7fe355135c205f2cbef9da14f1
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-21 19:09:16 +0300

    Tomita files new

--- opinion_mining/authors.cxx
+++ opinion_mining/authors.cxx
@@ -2,11 +2,27 @@
 #GRAMMAR_ROOT S
 
 ProperName -> Word<h-reg1>+;
+Pun -> "." | "," | "?" | "!" | ";" | ":" | "–" | "»" | QuoteDbl | QuoteSng | LBracket | RBracket| Hyphen;
+AdjP -> Adv* Adj<rt>;
+PP -> Prep NP;
+AuthorGr -> PP | NP;
 Organization -> Adj<gnc-agr[1]>* Noun<gnc-agr[1]>* Word<quoted,gnc-agr[1]>;
 Organization -> Adj<gnc-agr[1]>* Noun<gnc-agr[1]>* Word<lat>+;
-Author -> Organization | ProperName | Adj<gnc-agr[1]>* Noun<gnc-agr[1],gram="им">;
+//VP -> Adv* Verb<rt> NP<gram="~nom">*;
+VP -> Adv* Verb<rt> PP* NP<gram="~nom">*;
+Head -> Noun | ProperName;
+NP -> AdjP<gnc-agr[1]>* Head<gnc-agr[1]> AuthorGr*;
+Author -> NP;
 // Verb_speech -> Verb<kwtype="Speech">;
 //S -> Author interp (Author.Name) AnyWord* "SPEECH";
 //S -> "END_SPEECH" AnyWord* Author interp (Author.Name);
-S -> Author interp (Author.Name) AnyWord* "^";
-S -> Dollar AnyWord* Author interp (Author.Name);
+//S -> Author interp (Author.Name) VP* "^";
+//S -> Dollar VP* Author interp (Author.Name);
+S -> "~" Pun* VP NP interp (Author.Name);
+//RandomWord -> Word | Word Word;
+S -> NP interp (Author.Name) VP Pun* (Word) "^";
+//S -> NP;
+//S -> "~";
+//S -> VP NP;
+
+//S -> Pun+;
