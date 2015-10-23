25a9e18702e6de0fad9dd9a75fd846ae3e5dbf72
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-23 14:13:36 +0300

    Linux Tomita added

---
+++ Tomita/authors.cxx
@@ -0,0 +1,31 @@
+#encoding "utf-8"
+#GRAMMAR_ROOT S
+
+ProperName -> AnyWord<h-reg1>+;
+Pun -> "." | "," | "?" | "!" | ";" | ":" | "–" | "»" | QuoteDbl | QuoteSng | LBracket | RBracket| Hyphen | "—";
+AdjP -> Adv* Adj<rt>;
+PP -> Prep NP;
+ComplP -> PP | NP;
+Organization -> Adj<gnc-agr[1]>* Noun<gnc-agr[1]>* Word<quoted,gnc-agr[1]>;
+Organization -> Adj<gnc-agr[1]>* Noun<gnc-agr[1]>* Word<lat>+;
+//VP -> Adv* Verb<rt> NP<gram="~nom">*;
+VP -> Adv* Verb<rt> PP* NP<gram="~nom">*;
+Head -> Noun | ProperName | Organization;
+NP -> AdjP<gnc-agr[1]>* Head<gnc-agr[1], rt> ComplP<gram="~acc">*;
+//Author -> NP;
+// Verb_speech -> Verb<kwtype="Speech">;
+//S -> Author interp (Author.Name) AnyWord* "SPEECH";
+//S -> "END_SPEECH" AnyWord* Author interp (Author.Name);
+//S -> Author interp (Author.Name) VP* "^";
+//S -> Dollar VP* Author interp (Author.Name);
+S -> "~" Pun* VP<sp-agr[1]> NP<sp-agr[1]> interp (Author.Name);
+//RandomWord -> Word | Word Word;
+S -> NP<sp-agr[1]> interp (Author.Name) VP<sp-agr[1]> Pun* (Word) "^";
+S -> NP interp (Author.Name) Pun* (Word) "^";
+S -> "~" Pun* VP Prep NP interp (Author.Name);
+//S -> "по" NP interp (Author.Name) Pun+ "^";
+//S -> NP;
+//S -> "~";
+//S -> VP NP;
+
+//S -> Pun+;
